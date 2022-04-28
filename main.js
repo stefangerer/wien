54574b1257a7eb60d865d83bcc81d0000b6a3591
Stefan Angerer <angerer.st@gmail.com>
2022-04-28 17:32:27 +0200

    layer hinzufügen, map einrichten

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ let stephansdom = {
    lng: 16.373449188835192,
    titel: "Stephansdom",  
 }; 
+
+let startLayer = L.tileLayer.provider("BasemapAT.grau"); 
+
+let map = L.map("map", {
+    center: [stephansdom.lat, stephansdom.lng], 
+    zoom: 12, 
+    layers: [
+        startLayer
+    ]
+}); 
+
+let layerControl = L.control.layers({
+    "BasemapAT Grau": startLayer
+}).addTo(map);
+
+
